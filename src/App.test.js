b19f44b5b2e0b220aa853a906d53d1df5929d927
Neal Chandra <neal@ubiquity6.com>
2018-04-12 02:20:04 -0700

    Add fetch mock in tests so they pass in node without calling the API

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// fetch is not supported in node, which is the context in which these jest tests are run
+// this means that the external API calls won't work. That's OK though, we probably don't
+// want to rely on real API calls for our tests, so we can use this to stub out a dummy response.
+
+// keep in mind you will need to edit this mock response with the appropriate mock API data
+// so that your components recieve the data they expect.
+beforeAll(function() {
+  global.fetch = jest.fn().mockImplementation((url, options) => {
+      return new Promise((resolve, reject) => {
+        resolve({
+          ok: true,
+          Id: '123',
+          json: function() {
+            return {
+              // YOUR MOCK RESPONSE HERE
+            }
+          }
+        });
+      });
+  });
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
